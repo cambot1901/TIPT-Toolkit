7a39361d3f08bc0215ddd5fe962f08b4f886925f
cambot1901 <cambot4@gmail.com>
2025-06-05 18:38:05 +1000

    Delete frontend directory

--- frontend/src/app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-        <div>
-            <h1>Welcome to the TIPT Toolkit</h1>
-            <p>This is the home page. Use the menu to navigate.</p>
-        </div>
-    );
-}
